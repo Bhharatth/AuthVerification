b63b9bd360acef0db48d5d908ccfa9dbbfc8e276
bharath <bharathprasannan96@gmail.com>
2023-05-11 07:46:57 +0530

    added controlls for black list users and block users

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,8 @@
 import AsyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
 import Jobs from "../models/jobModel.js";
+import Employee from "../models/employeeModel.js";
+import Employer from "../models/employerModel.js";
 
 // @DESC gets the profile of the admin
 // @METHOD get
@@ -97,5 +99,128 @@ export const getAllEmployers = AsyncHandler(async (req, res) => {
       emailVerified: true,
     }).populate("employerData");
     res.status(200).json(allEmployers);
-  } catch (error) {}
+  } catch (error) {
+    res.status(403);
+    throw new Error(error);
+  }
+});
+
+// @DESC gets all blocked users
+// @METHOD get
+// @PATH /admin/blockedUsers
+// @QUERY keyword(name of the user)
+
+export const getAllBlockedUsers = AsyncHandler(async (req, res) => {
+  const keyword = req.query.keyword;
+
+  const query = keyword
+    ? {
+        name: {
+          $regex: keyword,
+          $options: "i",
+        },
+      }
+    : {};
+
+  try {
+    const blockedUsers = await User.find({
+      ...query,
+      isBloked: true,
+    })
+      .populate("AdminData")
+      .populate("employerData")
+      .populate("employeeData");
+
+    if (blockedUsers) {
+      res.send(201).json(blockedUsers);
+    } else {
+      res.status(200).json("no blocked users");
+    }
+  } catch (error) {
+    res.status(203);
+    throw new Error(error);
+  }
+});
+
+// @DESC Admin can blocked users
+// @METHOD post
+// @PATH /admin/block/:id
+// @PARAMS ID(id of the user)
+
+export const blockUsers = AsyncHandler(async (req, res) => {
+  const { _id } = req.params;
+  try {
+    const user = User.findById(_id);
+
+    if (user.userType === "employee") {
+      const employee = await Employee.findOne({ owner: _id });
+      const newNotification = new Notification({
+        owner: employee._id,
+        message: "your account have been blocked",
+      });
+
+      employee.notification.push(newNotification._id);
+      await newNotification.save();
+      await employee.save();
+    } else if (userType === "employer") {
+      const employer = await Employer.findOne({ owner: _id });
+      const newNotification = new Notification({
+        owner: employer._id,
+        message: "your account have been blocked",
+      });
+      employer.notification.push(newNotification._id);
+      await newNotification.save();
+      await employer.save();
+    }
+    user.isBloked !== user.isBloked;
+    user.save();
+
+    sendMail({
+      to: user.email,
+      from: "admin@gmail.com",
+      subject: "update alert",
+      html: `<div>
+            <h1>User update alert<h1/>
+            <p>your account have been blicked , contact admin for further infi<p/>
+            <div/>`,
+    });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(403);
+    throw new Error(error);
+  }
+});
+
+// @DESC Admin can blacklist usesrs
+// @METHOD post
+// @PATH /admin/blacklist
+// @PARAMS id(id of the bkacklisted user)
+
+export const blacklistUsers = AsyncHandler(async (req, res) => {
+  const { id } = req.body;
+
+  try {
+    const adminData = await Users.findById("12345").populate(
+      "blackListedUsers"
+    ); //default admin id
+    let a = 0;
+
+    //checking user will blacklisted wheater already in blacklisted users
+    adminData.blacklistUsers.forEach((e) => {
+      if (e + "*" === id + "*") {
+        a = 2;
+      }
+    });
+
+    if ((a = 0)) {
+      await adminData.blacklistUsers.push(_id);
+      adminData.save();
+      res.status(201).json(adminData);
+    } else {
+      res.status(403).json("user already in blocked list");
+    }
+  } catch (error) {
+    res.status(403);
+    throw new Error(error);
+  }
 });
